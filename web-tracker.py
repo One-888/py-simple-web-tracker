cd0dd80c007013462937ab63dba76d78c99e1ac5
One-888 <53633937+One-888@users.noreply.github.com>
2022-10-18 12:19:26 -0400

    Add clear web page

--- web-tracker.py
+++ web-tracker.py
@@ -4,15 +4,28 @@ import time
 
 
 def show_page(url, time_seconds):
+    split_3_part = time_seconds/3
     webbrowser.open(url)
-    time.sleep(time_seconds)
+    time.sleep(split_3_part)
+    print('\r**   ', end=' ')
+    time.sleep(split_3_part)
+    print('\r**** ', end=' ')
+    time.sleep(split_3_part)
+    print('\r******', end=' ')
     keyboard.press_and_release('ctrl+w')
+
+
+def clear_page(number_of_pages):
+    webbrowser.open(r'www.google.com')
+    for i in range(number_of_pages):
+        time.sleep(1)
+        keyboard.press_and_release('ctrl+w')
+    time.sleep(1)
 
 
 url_list = [r'https://www.kayak.com/flights/BKK-PIT/2022-10-27?sort=bestflight_a',
             r'https://www.skyscanner.net/transport/flights/BKKT/PIT/221027?adultsv2=1&cabinclass=economy&childrenv2=&currency=USD&rtn=0'
             r'https://www.orbitz.com/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from:BKK,to:Pittsburgh+(PIT+-+Pittsburgh+Intl.),departure:10/27/2022TANYT&options=cabinclass:economy&passengers=children:0,adults:1,seniors:0,infantinlap:Y&fromDate=10/27/2022&d1=2022-10-27',
-            r'https://www.united.com/en/us/fsr/choose-flights?f=BKK&t=PIT&d=2022-10-26&tt=1&sc=7&px=1&taxng=1&newHP=True&clm=7&st=bestmatches&tqp=R&fareWheel=False',
             r'https://www.kayak.com/flights/BKK-PIT/2022-10-27?sort=bestflight_a&fs=airlines=~AA',
             r'https://www.kayak.com/flights/BKK-PIT/2022-10-27?sort=bestflight_a&fs=airlines=~DL',
             r'https://www.kayak.com/flights/BKK-PIT/2022-10-27?sort=bestflight_a&fs=airlines=~UA',
@@ -21,8 +34,10 @@ url_list = [r'https://www.kayak.com/flights/BKK-PIT/2022-10-27?sort=bestflight_a
 
 print('Start in 15 seconds...')
 time.sleep(15)
+print('Go..')
 reload_seconds = 30
 
+clear_page(10)
 for i in range(10):
     for url in url_list:
         show_page(url, reload_seconds)
